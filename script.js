182fdcec094cd40347ab9f6dd30248ff16da123f
levure-s <levure.sauvage@gmail.com>
2021-02-21 09:37:25 +0900

    add_sellToken

--- script.js
+++ script.js
@@ -18,8 +18,7 @@ contract.events.buyToken({}, (err,event) => {
             let hash = crypto.createHash('sha256').update(data).digest()
             console.log(hash.toString("hex"))
             let wallet = new Wallet(hash)
-            var index = parseInt(crypto.randomBytes(16).toString("hex"))  % 9 + 1
-            console.log(index)
+            var index = 1
             var address0 = wallet.getAddress(0)
             console.log(address0)
             wallet.sendCoins(0,index,parseInt(value)*1e8).then((result)=>{
@@ -29,7 +28,39 @@ contract.events.buyToken({}, (err,event) => {
                 }else{
                     console.log("Bitcoin_tx:false")
                 }
+            }).catch((err)=>{
+                console.log(err)
+            })            
+        }).catch((err)=>{
+            console.log(err)
+        });
+        
+    } else {
+        console.log(err);
+    }
+});
+
+contract.events.sellToken({}, (err,event) => {
+    if(!err) {
+		var addr = event.returnValues._sender;
+		var value = event.returnValues._value;
+        console.log("[notice] SellToken! (addr:" + addr + " ,value:" + value + ")");
+        web3.eth.getAccounts().then((res)=>{
+            let data = Buffer.from(addr + res[0], 'utf8')
+            let hash = crypto.createHash('sha256').update(data).digest()
+            console.log(hash.toString("hex"))
+            let wallet = new Wallet(hash)
+            var index = 1
+            var address0 = wallet.getAddress(0)
+            console.log(address0)
+            wallet.sendCoins(index,0,parseInt(value)*1e8).then((result)=>{
+                console.log(result)
+            }).catch((err)=>{
+                console.log(err)
+                contract.methods.transfer(addr,value).send({from:res[0], gas: '5000000'});
             })           
+        }).catch((err)=>{
+            console.log(err)
         });
         
     } else {
